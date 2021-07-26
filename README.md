a81dddd28161a5151291c1792399106b8fe757d6
Iron512 <ivanmartini96@gmail.com>
2021-07-26 14:53:54 +0000

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# SmithWatermanAlgorithm
-A python implementation of the Smith Waterman Algorithm. Algorithms for BioInformatics 2020/2021 @ Unitn
+# Smith Waterman Algorithm
+This repository contains all the material regarding the final project for the course of Algorithms for bioinformatics, for the year 2020/2021.
+The task is an implementation of the Smith Waterman Algorithm (provided here in python)
+
+Ivan Martini #2075971
+
+### Setup
+0) **Prerequisites**: Have `python` installed. Nothing more should be required on any operating system. I had the chance to test the code only on Archlinux and Ubuntu 20.04, but everything went fine.
+
+1) Clone the repository
+```
+git clone https://github.com/iron512/DataMining2021.git
+```
+
+2) Run the code
+```
+python3 smith_waterman.py TGTTGTAG TGTTGAT
+```
+
+*Note: the script has an helper. using the command -h it will show all the parameters that can be added to tune the algorithm*
+
+3) Examples
+Run the code changing the default scoring system (+3,-3,-2)
+```
+python3 smith_waterman.py TGTTACGG GGTTGACTA -sm +1 -sx -1 -sg -2
+```
+
+Run the code showing in the end the tracing matrix
+```
+python3 smith_waterman.py TGTTACGG GGTTGACTA -m std
+```
+
+Run the code showing in the end the tracing matrix, highlighting also the path in yellow. suitable for a terminal display, but not for storing on a txt file (use -m std instead)
+```
+python3 smith_waterman.py TGTTACGG GGTTGACTA -m color
+```
--- smith_waterman.py
+++ smith_waterman.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python
 
+#TGTTACGG GGTTGACTA
+
 import argparse
-import numpy
 
-def cmatch(a, b):
+#auxiliary function for applying the colors
+class bcolors:
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	CYAN = '\033[96m'
+	RED = '\033[91m'
+	RESET = '\033[0m'
+
+def grn(text):
+	return bcolors.GREEN + str(text) + bcolors.RESET
+
+def ylw(text):
+	return bcolors.YELLOW + str(text) + bcolors.RESET
+
+def red(text):
+	return bcolors.RED + str(text) + bcolors.RESET
+
+#the custom match function. Can be updated to provide differents rules other than match and mistmatch.
+def cmatch(a, b, positive, negative):
 	if a == b:
-		return +3
+		return positive
 	else:
-		return -3
+		return negative
 
-def pprint(s1,s2,matrix):
+#the custom print function. The first row/columns are substituted by the actual letters of the sequences.
+#online versions also prompt the first rows/columns (filled with zeros) but I feel that this way of displaying the 
+#result is more intuitive and understandable. 
+def pprint(s1, s2, matrix):
 	for i in range(0, len(s1)+1):
 		for j in range(0, len(s2)+1):
 			if i == 0 and j == 0:
-				print(0, end="\t")
+				print("\\", end="\t")
 			elif i == 0:
 				print(s2[j-1], end="\t")
 			elif j == 0:
@@ -22,9 +44,27 @@ def pprint(s1,s2,matrix):
 				print(matrix[i][j], end="\t")		
 		print()
 
+#A little modification of the function presented above. The headers are colored in green, but also the actual sequence is highlighted in yellow.
+#Since it was quite hard to display the arrows in the terminal, I adopted this solution.
+#It works using the terminal colors. Not suited for storage in txt files (-m std fits it better.)
+def pprint_color(s1, s2, matrix, color):
+	for i in range(0, len(s1)+1):
+		for j in range(0, len(s2)+1):
+			if i == 0 and j == 0:
+				print(grn("\\"), end="\t")
+			elif i == 0:
+				print(grn(str(s2[j-1])), end="\t")
+			elif j == 0:
+				print(grn(str(s1[i-1])), end="\t")
+			else:
+				if (i,j) not in color:
+					print(matrix[i][j], end="\t")
+				else:
+					print(ylw(str(matrix[i][j])), end="\t")	
+		print()
 
 def main():
-	#SETUP THE PARSER
+	#setup the parser
 	parser = argparse.ArgumentParser(description = "A basic implementation of the smith-waterman algorithm for the local alignment.\nThis code has been developed as part of the final exam of the course of Algorithms for Bioinformatics 2020/2021, University of Trento",formatter_class=argparse.RawTextHelpFormatter, add_help=False)
 	parser.add_argument("s1", type=str, help="the first sequence, coded as a String")
 	parser.add_argument("s2", type=str, help="the second sequence, coded as a String")
@@ -35,11 +75,12 @@ def main():
 	parser.add_argument("-sx","--scoring_mismatch",type = int, help="The mismatch value for the custom storing system (default = -3)\n", default = -3)
 	parser.add_argument("-sg","--scoring_gap",type = int, help="The gap value for the custom storing system (default = -2)\n\n", default = -2)
 
-	#parser.add_argument("-m","--matrix", help="a file CSV containing the PAM matrix wanted")
+	parser.add_argument("-m","--matrix", help="Show the traceback matrix.\n", default='none', choices=['none', 'std', 'color'])
 
-	#PARSE THE USER'S INPUT
+	#parse the user's input
 	args = parser.parse_args()
 
+	#transform the input in an uppercase array of chars
 	seq1 = [char for char in args.s1.upper()]
 	seq2 = [char for char in args.s2.upper()]
 
@@ -47,26 +88,42 @@ def main():
 	mismatch = args.scoring_mismatch
 	gap = args.scoring_gap 
 
-	#TGTTACGG GGTTGACTA
-	matrix = numpy.zeros((len(seq1) + 1, len(seq2) + 1), dtype=object)
-	direction = numpy.zeros((len(seq1) + 1, len(seq2) + 1), dtype=object)
+	output = args.matrix
+
+	#initialize through numpy the required matrices
+	matrix = []
+	direction = []
+	for x in range(0,len(seq1)+1):
+		matrix.append([])
+		direction.append([])
+		for y in range(0,len(seq2)+1):
+			matrix[x].append(0)
+			direction[x].append([])
+
+
+	#store the maximal values in just one pass
 	current_max = -1
 	current_max_pos = []
 
 	for i in range(1, len(seq1)+1):
-		for j in range(1, len(seq2)+1):
-			direction[i][j] = []
-			
-			diag = matrix[i-1][j-1] + cmatch(seq1[i-1], seq2[j-1])  
-			up = matrix[i-1][j] -2
-			left = matrix[i][j-1] -2 
+		for j in range(1, len(seq2)+1):			
+			#generate the candidates
+			diag = matrix[i-1][j-1] + cmatch(seq1[i-1], seq2[j-1], match, mismatch)  
+			up = matrix[i-1][j] + gap
+			left = matrix[i][j-1] + gap 
 			new = 0
 
+			#store the highest candidate(s)
 			matrix[i][j] = max(diag, up, left, new)
+			
 			if matrix[i][j] > current_max:
 				current_max = matrix[i][j]
-				current_max_pos = [i,j]
-
+				current_max_pos = [[i,j]]
+			#Store all the highest values. Just ONE path each is explored
+			elif matrix[i][j] == current_max:
+				current_max_pos.append([i,j])
+			
+			#keep track of the candidates, needed for backtracking the algorithm.
 			if (matrix[i][j] == diag):
 				direction[i][j].append('d')
 			if (matrix[i][j] == up):
@@ -76,38 +133,65 @@ def main():
 			if (matrix[i][j] == new):
 				direction[i][j].append('n')
 
-	x = current_max_pos[0]
-	y = current_max_pos[1]
-
-	alignA = []
-	alignB = []
-
-	while matrix[x][y] != 0:
-		if direction[x][y][0] == 'd':		
-			alignA.append(seq1[x-1])
-			alignB.append(seq2[y-1])
-			x-=1
-			y-=1
-
-		elif direction[x][y][0] == 'u':	
-			alignA.append('-')
-			alignB.append(seq1[x-1])
-			x-=1
-
-		elif direction[x][y][0] == 'l':	
-			alignA.append(seq2[y-1])
-			alignB.append('-')
-			y-=1
-
-	pprint(seq1,seq2,matrix)
-
-	alignA.reverse()
-	alignB.reverse()
-	print("".join(alignA))
-	print("".join(alignB))
+	print("SEQ1:","".join(seq1))
+	print("SEQ2:","".join(seq2))
+	print("SCORE:",current_max)
 	print()
 
+	#the pair will always be one (The last, arbitrary choice), but the algorithm is ready to explore multiple paths.
+	for pair in current_max_pos[-1:]:
+		x = pair[0]
+		y = pair[1]
 
+		#store the backtracked values and their position (to color the path)
+		alignA = []
+		matches = []
+		alignB = []
+		color = []
+
+		#start from the end, go until a cell found has been inizialized by a "new" (or the boundaries are exceeded).
+		while x>0 and y>0 and (not direction[x][y][0] == 'n'):
+			#parse the match/mismatch
+			color.append((x,y))
+			if direction[x][y][0] == 'd':		
+				alignA.append(seq1[x-1])
+				alignB.append(seq2[y-1])
+				if (seq1[x-1] == seq2[y-1]):
+					matches.append("|")
+				else:
+					matches.append("X")
+				x-=1
+				y-=1
+
+			#parse the gaps
+			elif direction[x][y][0] == 'u':	
+				alignA.append('-')
+				alignB.append(seq1[x-1])
+				matches.append(" ")
+				x-=1
+
+			elif direction[x][y][0] == 'l':	
+				alignA.append(seq2[y-1])
+				alignB.append('-')
+				matches.append(" ")
+				y-=1
+		color.append((x,y))
+
+		#reverse the backtracked values, in order to produce the correct string and print them as a string
+		alignA.reverse()
+		matches.reverse()
+		alignB.reverse()
+
+		print("".join(alignA))
+		print("".join(matches))
+		print("".join(alignB))
+		print()
+
+		if output == "color":
+			pprint_color(seq1,seq2,matrix,color)
+
+	if output == "std":
+		pprint(seq1,seq2,matrix)
 
 #MAIN
 if __name__ == '__main__':
